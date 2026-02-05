5f2e092a0523d010131b7db69d9b27d20ca76611
Will Zhang <willwz@gmail.com>
2026-02-05 17:30:21 -0500

    ENH:rust: add Precomputable trait

--- rust/models/src/kinematics/mod.rs
+++ rust/models/src/kinematics/mod.rs
@@ -1,2 +1,8 @@
 pub mod deformation;
 pub mod invariants;
+use crate::utils::errors::PyError;
+use ndarray::ArrayView2;
+
+pub trait Precomputable {
+    fn precompute(&self, t_c: &ArrayView2<f64>) -> Result<(), PyError>;
+}
